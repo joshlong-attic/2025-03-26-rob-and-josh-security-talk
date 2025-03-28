84d489bce397796d6fc8d22d3f46d178dbf954cf
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-03-28 17:35:30 -0500

    Use RestClient + RouterFunction<ServerResponse>

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -2,75 +2,49 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.server.mvc.filter.TokenRelayFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.servlet.function.HandlerFunction;
 import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
-import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.security.oauth2.client.web.client.RequestAttributeClientRegistrationIdResolver.clientRegistrationId;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 
 @SpringBootApplication
 public class ClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
 
+	@Bean
+	public RestClient restClient(RestClient.Builder builder, OAuth2AuthorizedClientManager authorizedClientManager) {
+		OAuth2ClientHttpRequestInterceptor requestInterceptor =
+				new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
+		requestInterceptor.setClientRegistrationIdResolver(request -> "spring");
+
+		return builder.requestInterceptor(requestInterceptor).build();
+	}
 
 }
 
 @Configuration
-class GatewayConfiguration {
+class RouterConfiguration {
 
-    @Bean
-    RouterFunction<ServerResponse> routerFunction(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient oAuth2AuthorizedClient) {
-        return route()
-                .filter(TokenRelayFilterFunctions.tokenRelay())
-                .GET("/**", http("http://service.local.gd:8081"))
-                .GET("/**", new HandlerFunction<ServerResponse>() {
-                    @Override
-                    public ServerResponse handle(ServerRequest request) throws Exception {
-                        return null;
-                    }
-                })
-                .build();
-    }
-}
-
-
-///
-/// todo show how to autoconfigure the interceptor
-//@Controller
-@ResponseBody
-class ClientController {
-
-    private final RestClient restClient;
-
-    ClientController(RestClient.Builder restClient) {
-        this.restClient = restClient.build();
-    }
-
-    @GetMapping("/")
-    ResponseEntity<?> index(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
-        return this.restClient
-                .get()
-                .uri("http://service.local.gd:8081")
-                .headers(http -> http
-                        .setBearerAuth(authorizedClient.getAccessToken().getTokenValue())
-                )
-                .retrieve()
-                .toEntity(String.class);
-    }
+	@Bean
+	RouterFunction<ServerResponse> routerFunction(RestClient rest) {
+		return route()
+			.GET("/**", request -> {
+				String body = rest.get()
+					.uri("http://service.local.gd:8081")
+					.retrieve()
+					.body(String.class);
+				return ServerResponse.ok().body(body);
+			})
+			.build();
+	}
 }
