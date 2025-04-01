e3adc1725d6916b4b3f73e9aa9d955220b9af5e0
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-03-31 22:38:18 -0500

    @authz.check

---
+++ service/src/main/java/com/example/service/Authz.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.security.authorization.AuthorityAuthorizationManager.hasRole;
+
+@Service
+public class Authz {
+	public boolean check(Authentication authentication, BankAccount account) {
+		if (authentication.getName().equals(account.getOwner())) {
+			return true;
+		}
+		return hasRole("ACCOUNTANT").authorize(() -> authentication, account).isGranted();
+	}
+}
