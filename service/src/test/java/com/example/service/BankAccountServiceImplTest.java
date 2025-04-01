a2aa69eb1cbe6d7e72a56b37a2b2da3c654a9799
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-03-31 22:13:21 -0500

    @WithMockUser

--- service/src/test/java/com/example/service/BankAccountServiceImplTest.java
+++ service/src/test/java/com/example/service/BankAccountServiceImplTest.java
@@ -1,16 +1,11 @@
 package com.example.service;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.authorization.AuthorizationProxyFactory;
-import org.springframework.security.authorization.method.AuthorizationAdvisorProxyFactory;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -20,26 +15,15 @@ class BankAccountServiceImplTest {
 	@Autowired
 	BankAccountService account;
 
-	void login(String user) {
-		TestingAuthenticationToken auth =
-			new TestingAuthenticationToken(user, null, "ROLE_USER");
-		SecurityContextHolder.setContext(new SecurityContextImpl(auth));
-	}
-
-	@AfterEach
-	void cleanup() {
-		SecurityContextHolder.clearContext();
-	}
-
 	@Test
+	@WithMockUser("rob")
 	void findByIdWhenGranted() {
-		login("rob");
 		this.account.findById(1);
 	}
 
 	@Test
+	@WithMockUser("josh")
 	void findByIdWhenDenied() {
-		login("josh");
 		assertThatExceptionOfType(AccessDeniedException.class)
 			.isThrownBy(() -> this.account.findById(1));
 	}
