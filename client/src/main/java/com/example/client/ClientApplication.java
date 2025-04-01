1554a4951d9c2e0a5cef87c3b88c615a64de02a7
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-03-31 21:50:06 -0500

    TokenRelayFilters + Spring Cloud

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -2,15 +2,13 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.server.mvc.filter.TokenRelayFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
-import static org.springframework.security.oauth2.client.web.client.RequestAttributeClientRegistrationIdResolver.clientRegistrationId;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 
@@ -21,30 +19,16 @@ public class ClientApplication {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
-	@Bean
-	public RestClient restClient(RestClient.Builder builder, OAuth2AuthorizedClientManager authorizedClientManager) {
-		OAuth2ClientHttpRequestInterceptor requestInterceptor =
-				new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
-		requestInterceptor.setClientRegistrationIdResolver(request -> "spring");
-
-		return builder.requestInterceptor(requestInterceptor).build();
-	}
-
 }
 
 @Configuration
 class RouterConfiguration {
 
 	@Bean
-	RouterFunction<ServerResponse> routerFunction(RestClient rest) {
+	RouterFunction<ServerResponse> routerFunction() {
 		return route()
-			.GET("/**", request -> {
-				String body = rest.get()
-					.uri("http://service.local.gd:8081")
-					.retrieve()
-					.body(String.class);
-				return ServerResponse.ok().body(body);
-			})
+			.filter(TokenRelayFilterFunctions.tokenRelay())
+			.GET("/**", http("http://service.local.gd:8081"))
 			.build();
 	}
 }
